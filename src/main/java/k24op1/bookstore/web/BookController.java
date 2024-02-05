7b734777519934bc074ef0c812fb0e19486431b7
TerhiElina <terhi.evakallio@gmail.com>
2024-02-05 20:45:27 +0200

    Bookstore: Listpage

--- src/main/java/k24op1/bookstore/web/BookController.java
+++ src/main/java/k24op1/bookstore/web/BookController.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.ArrayList;
+
 
 
 @Controller
@@ -12,11 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BookController {
     @Autowired
     private BookRepository repository;
-    @RequestMapping(value={"/","index"})
+    @RequestMapping(value={"/booklist"})
     public String bookList(Model model){
         model.addAttribute("books", repository.findAll());
-        return "index";
+        return "booklist";
     }
+    
     }
     
     /*public String getBooks (Model model){
